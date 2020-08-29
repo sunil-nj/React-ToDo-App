fe5b7fa772d541ea517e38a8ef5d1b33c6f5589c
sunil <sunil.kumarnj66@gmail.com>
2020-08-29 19:07:22 +0530

    Updates

---
+++ precache-manifest.b20bd811f14ab183f433f9aa5cec8afe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf139ce6b16a19622b1f7c31647c886",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "97650ad9ad8f144bf45a",
+    "url": "/React-ToDo-App/static/css/main.4146d708.chunk.css"
+  },
+  {
+    "revision": "02153e79894b663365c5",
+    "url": "/React-ToDo-App/static/js/2.6a80fce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.6a80fce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97650ad9ad8f144bf45a",
+    "url": "/React-ToDo-App/static/js/main.19ec9170.chunk.js"
+  },
+  {
+    "revision": "1a118a873dfd5e6996ad",
+    "url": "/React-ToDo-App/static/js/runtime-main.2177da3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
